3dd4d3f8034532f556602608b7a95f57d2702aa3
Bryant Lee <leeisefforts@163.com>
2019-01-01 03:27:32 +0800

    'add'

--- common/model/baoming.py
+++ common/model/baoming.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.schema import FetchedValue
+from common.model.department import Department
+from common.model.profession import Profession
 from application import db
 
 
@@ -15,3 +17,33 @@ class BaoMing(db.Model):
     f4 = db.Column(Integer, nullable=False, server_default=FetchedValue())
     f0 = db.Column(Integer, nullable=False, server_default=FetchedValue())
     openid = db.Column(String(128), nullable=False, server_default=FetchedValue())
+
+    @property
+    def sid_desc(self):
+        pro = Profession.query.order_by(Profession.pid.desc()).first()
+        return pro.p_name
+
+    @property
+    def f0_desc(self):
+        de = Department.query.filter_by(did=self.f0).first()
+        return de.d_name
+
+    @property
+    def f1_desc(self):
+        de = Department.query.filter_by(did=self.f1).first()
+        return de.d_name
+
+    @property
+    def f2_desc(self):
+        de = Department.query.filter_by(did=self.f2).first()
+        return de.d_name
+
+    @property
+    def f3_desc(self):
+        de = Department.query.filter_by(did=self.f3).first()
+        return de.d_name
+
+    @property
+    def f4_desc(self):
+        de = Department.query.filter_by(did=self.f4).first()
+        return de.d_name
--- web/controller/admin/__init__.py
+++ web/controller/admin/__init__.py
@@ -1,10 +1,31 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from common.libs.WebHelper import ops_render
+from common.model.admin import Admin
 
 route_admin = Blueprint('admin_page', __name__)
+from web.controller.admin.index import *
+from web.controller.admin.manager import *
+from web.controller.admin.profession import *
 
 
 @route_admin.route('/', methods=['GET', 'POST'])
+@route_admin.route('/login', methods=['GET', 'POST'])
 def admin():
+    resp = {'code': 200, 'msg': '登录成功'}
     if request.method == 'GET':
         return ops_render('/admin/index.html')
+
+    req = request.values
+    name = req['name'] if 'name' in req else ''
+    pwd = req['pwd'] if 'pwd' in req else ''
+
+    admin = Admin.query.filter_by(username=name).first()
+    admin = Admin.query.filter_by(password=pwd).first()
+    resp['id'] = admin.id
+    if admin:
+        return jsonify(resp)
+
+    resp['code'] = -1
+
+    resp['msg'] = '用户名或密码错误'
+    return jsonify(resp)
